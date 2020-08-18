4d50582e7d4e4dd99ea75b5b14f8b01255e50af9
Justin Liu <justinliu4234@utexas.edu>
2020-08-18 00:24:02 -0500

    first test in odrive successful

--- odrive/first_test.py
+++ odrive/first_test.py
@@ -9,45 +9,67 @@ import odrive
 from odrive.enums import *
 import time
 import math
+import turtle
 
 # Find a connected ODrive (this will block until you connect one)
 print("finding an odrive...")
-my_drive = odrive.find_any()
-
-# Find an ODrive that is connected on the serial port /dev/ttyUSB0
-#my_drive = odrive.find_any("serial:/dev/ttyUSB0")
+odrv0 = odrive.find_any()
 
 # Calibrate motor and wait for it to finish
 print("starting calibration...")
-my_drive.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-while my_drive.axis1.current_state != AXIS_STATE_IDLE:
+odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while odrv0.axis1.current_state != AXIS_STATE_IDLE and odrv0.axis0.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
-my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+odrv0.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
 
 # To read a value, simply read the property
-print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
+print("Bus voltage is " + str(odrv0.vbus_voltage) + "V")
 
 # Or to change a value, just assign to the property
-my_drive.axis1.controller.pos_setpoint = 3.14
-print("Position setpoint is " + str(my_drive.axis1.controller.pos_setpoint))
+odrv0.axis1.controller.pos_setpoint = 3.14
+print("Position setpoint is " + str(odrv0.axis1.controller.pos_setpoint))
 
 # And this is how function calls are done:
 for i in [1,2,3,4]:
-    print('voltage on GPIO{} is {} Volt'.format(i, my_drive.get_adc_voltage(i)))
+    print('voltage on GPIO{} is {} Volt'.format(i, odrv0.get_adc_voltage(i)))
 
-# A sine wave to test
-t0 = time.monotonic()
-while True:
-    setpoint = 10000.0 * math.sin((time.monotonic() - t0)*2)
-    print("goto " + str(int(setpoint)))
-    my_drive.axis1.controller.pos_setpoint = setpoint
-    time.sleep(0.01)
+# # A sine wave to test
+# t0 = time.monotonic()
+# while True:
+#     setpoint = 10000.0 * math.sin((time.monotonic() - t0)*2)
+#     print("goto " + str(int(setpoint)))
+#     odrv0.axis1.controller.pos_setpoint = setpoint
+#     time.sleep(0.01)
 
-# Some more things you can try:
+def increase():
+    print("increasing speed")
+    odrv0.axis0.controller.vel_setpoint = 10000
+    
+def decrease():
+    print("decreasing speed")
+    odrv0.axis0.controller.vel_setpoint = -10000
 
-# Write to a read-only property:
-my_drive.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
+def nocrease():
+    print("no speed")
+    odrv0.axis0.controller.vel_setpoint = 0
 
-# Assign an incompatible value:
-my_drive.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
+tim = turtle.Turtle()
+print("listening for arrow keys now....")
+turtle.listen()
+
+turtle.onkeypress(increase, "Right")
+turtle.onkeypress(decrease, "Left")
+turtle.onkeyrelease(nocrease, "Right")
+turtle.onkeyrelease(nocrease, "Left")
+
+turtle.mainloop()
+
+
+
+
